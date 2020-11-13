48c3a0b954397f45a6fc81ebbcfcca2fc2c587d1
Andrei Safonov <klim1286@gmail.com>
2020-11-13 23:01:08 +0300

    add unit 2-2

---
+++ unit2-2.py
@@ -0,0 +1,9 @@
+elements = input('Введите элементы списка через пробел').split()
+i = 0
+for el in range(len(elements) // 2):
+    el1 = elements[i]
+    el2 = elements[i + 1]
+    elements[i] = el2
+    elements[i + 1] = el1
+    i += 2
+print(elements)
