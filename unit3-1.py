393f99cfedf03c35d5dd583e4ba7dd2efab8e2fd
Andrei Safonov <klim1286@gmail.com>
2020-11-17 15:45:28 +0300

    add unit3-1

---
+++ unit3-1.py
@@ -0,0 +1,17 @@
+def split(a1,a2):
+    sp = a1 / a2
+    sp = round(sp, 5)
+    return sp
+
+while True:
+    try:
+        a1 = float(input(f'Введите первое число: '))
+        a2 = float(input(f'Введите второе число: '))
+        print(f'Результат деления {split(a1,a2)}')
+        break
+    except ZeroDivisionError:
+        print('На ноль делить нельзя!')
+        continue
+    except ValueError:
+            print('Невозможно преобразовать строку в число!')
+            continue
