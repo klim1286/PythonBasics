e0fd93c39978a4662b170eaea8bf2a9303d78208
Andrei Safonov <klim1286@gmail.com>
2020-12-08 00:01:42 +0300

    update unit8-456

--- unit8-456.py
+++ unit8-456.py
@@ -1,4 +1,17 @@
 loc = {1: 'Бухгалтерия', 2: 'Финансисты', 3: 'Директор'}
+model_print = {1: 'HP LaserJet P2035', 2: 'HP LaserJet Pro M203dn', 3: 'HP LaserJet Pro M15w',
+               4: 'KYOCERA FS-1040', 5: 'KYOCERA ECOSYS P2040dn', 6: 'KYOCERA ECOSYS P2335dw',
+               7: 'Canon i-SENSYS LBP112', 8: 'Canon i-SENSYS LBP226dw', 9: 'Canon i-SENSYS LBP162dw'
+               }
+model_scan = {1: 'Fujitsu ScanPartner SP1120', 2: 'Fujitsu fi-7260', 3: 'Fujitsu SP-1425',
+              4: 'HP ScanJet Pro 2000 s2', 5: 'HP ScanJet Pro 2500 f1', 6: 'HP ScanJet Pro N4000 snw1',
+              7: 'Brother ADS-2200', 8: 'Brother ADS-3000N', 9: 'Brother ADS-3600W'
+              }
+model_xerox = {1: 'CANON imageRUNNER 2204', 2: 'Kyocera TASKalfa 181 1102KJ3NL0', 3: 'CANON imageRUNNER C3025',
+               4: 'Xerox WorkCentre Copier 5230', 5: 'CANON imageRUNNER C3025i', 6: 'Xerox WorkCentre 6400X',
+               7: 'HP Color LaserJet Enterprise 700 M775f', 8: 'CANON imageRUNNER 1435iF',
+               9: 'Canon imageRUNNER C1225IF'
+               }
 
 
 class Warehous:
@@ -19,13 +32,13 @@ class Warehous:
         printer_out, scaner_out, xerox_out = [], [], []
         for count in self.data:
             if count['equipment'] == 'printer' and count['location'] == 'sklad':
-                printer_out.append(f'ID {self.data.index(count)}, Модель: {count["model"]}, Цена: {count["cost"]}\n')
+                printer_out.append(f'ID: {self.data.index(count)}, Модель: {count["model"]}, Цена: {count["cost"]}\n')
                 printer += 1
             elif count['equipment'] == 'scaner' and count['location'] == 'sklad':
-                scaner_out.append(f'ID {self.data.index(count)}, Модель: {count["model"]}, Цена: {count["cost"]}\n')
+                scaner_out.append(f'ID: {self.data.index(count)}, Модель: {count["model"]}, Цена: {count["cost"]}\n')
                 scaner += 1
             elif count['equipment'] == 'xerox' and count['location'] == 'sklad':
-                xerox_out.append(f'ID {self.data.index(count)}, Модель: {count["model"]}, Цена: {count["cost"]}\n')
+                xerox_out.append(f'ID: {self.data.index(count)}, Модель: {count["model"]}, Цена: {count["cost"]}\n')
                 xerox += 1
         return f'**********************************************\n' \
                f'На складе находится: \n' \
@@ -35,7 +48,7 @@ class Warehous:
                f'{"".join(scaner_out)}\n' \
                f'Ксероксов: {xerox} шт. \n' \
                f'{"".join(xerox_out)}' \
-               f'**********************************************'\
+               f'**********************************************' \
 
 
     def moving_equ(self, id, location):
@@ -76,7 +89,7 @@ class Xerox(Equipment):
 
     def __init__(self, model, cost, resolution='1800*600'):
         super().__init__(model, cost)
-        self.resolution = resolution  # Разрешение копии
+        self.resolut1ion = resolution  # Разрешение копии
 
 
 printer1 = Printer('HP', 5000)
@@ -86,6 +99,8 @@ printer3 = Printer('Kyocera', 12990)
 xerox = Xerox('Canon', 3590)
 sklad = Warehous()
 sklad.equipments_in(printer1, scaner1, printer2, printer3)  # Оприходование техники на склад
+
+
 # sklad.moving_equ(2, 'accounting department')  # Передача техники
 # print(sklad.count_equ()) # Вывод техники на складе
 
@@ -108,47 +123,123 @@ def menu_1():
     return input('Выберете номер нужного пункта меню и нажмите Ввод: ')
 
 
+def print_mod_eq(mod_eq):
+    txt = ''
+    z = 0
+    for i in mod_eq:
+        if z == 5:
+            txt += '\n'
+            z = 0
+        txt += f'{i}:{mod_eq[i]} | '
+        z += 1
+    return txt
+
+
+def chose_eq(type_eq):
+    if type_eq == 1:
+        while True:
+            print(print_mod_eq(model_print))
+            type_mod = int(input("Выбирете модель цифрами: "))
+            if 1 > type_mod or type_mod > len(model_print):
+                print('Такой модели нет, выберете из списка:')
+            else:
+                cost = int(input("Введите стоимость оборудования: "))
+                return model_print[int(type_mod)], cost
+    elif type_eq == 2:
+        while True:
+            print(print_mod_eq(model_scan))
+            type_mod = int(input("Выбирете модель цифрами: "))
+            if 1 > type_mod or type_mod > len(model_print):
+                print('Такой модели нет, выберете из списка:')
+            else:
+                cost = int(input("Введите стоимость оборудования: "))
+                return model_scan[int(type_mod)], cost
+    elif type_eq == 3:
+        while True:
+            print(print_mod_eq(model_xerox))
+            type_mod = int(input("Выбирете модель цифрами: "))
+            if 1 > type_mod or type_mod > len(model_print):
+                print('Такой модели нет, выберете из списка:')
+            else:
+                cost = int(input("Введите стоимость оборудования: "))
+                return model_xerox[int(type_mod)], cost
+
+
 while True:
     try:
         mm = int(main_menu())
         if mm == 3:
             print(sklad.count_equ())
-            main_menu()
+            continue
         elif mm == 1:
-            n1 = int(menu_1())
+
             while True:
-                num = int(input('Какое количество принтеров вы хотите добавить? \nВведите число от 1 до 9: '))
-                if 0 < num < 10:
+                n1 = int(menu_1())
+                if n1 < 4 and n1 > 0:
+                    num = int(input('Какое количество оборудования вы хотите добавить? \nВведите число от 1 до 9: '))
+                    if 0 < num < 10:
+                        break
+                    else:
+                        print('Введите число от 1 до 9')
+                        continue
+                elif n1 == 0:
                     break
                 else:
-                    print('Введите число от 1 до 9')
                     continue
 
             for _ in range(num):
-                type_mod = input("Введите модель: ")
-                cost = input("Введите стоимость оборудования: ")
 
                 # Если выбран принтер
                 if n1 == 1:
-                    sklad.equipments_in(Printer(type_mod, cost))
+                    answer = chose_eq(n1)
+                    sklad.equipments_in(Printer(answer[0], answer[1]))
+                    print('Принтер успешно добавлен на склад')
                 # Если выбран сканер
                 elif n1 == 2:
-                    sklad.equipments_in(Scaner(type_mod, cost))
-
+                    answer = chose_eq(n1)
+                    sklad.equipments_in(Scaner(answer[0], answer[1]))
+                    print('Сканер успешно добавлен на склад')
                 # Если выбран ксерокс
                 elif n1 == 3:
-                    sklad.equipments_in(Xerox(type_mod, cost))
-
+                    answer = chose_eq(n1)
+                    sklad.equipments_in(Xerox(answer[0], answer[1]))
+                    print('Ксерокс успешно добавлен на склад')
                 # Если выбран выход или ошибочный ввод
+                elif n1 == 0:
+                    break
                 else:
                     continue
+
         elif mm == 2:
-            print(sklad.count_equ())
-            id = int(input('Введите ID оборудования которое нужно передать: '))
-            division = int(input(f'{loc} \nВведите номер подразделения для передачи оборудования:'))
-            sklad.moving_equ(id, division)
-        else:
+            while True:
+                print(sklad.count_equ())
+                id = int(input('Введите ID оборудования которое нужно передать: '))
+                if id < 0 or id > len(sklad.data) - 1:
+                    print('Введите существующий ID оборудования которое нужно передать: ')
+                    continue
+                elif sklad.data[id]['location'] != 'sklad':
+                    print('Оборудование уже переданно в другое подразделение\n'
+                          'Введите ID оборудования которое нужно передать: ')
+                    continue
+                else:
+                    break
+
+            while True:
+                division = int(input(f'{loc} \nВведите номер подразделения для передачи оборудования:'))
+                if division < 1 or division > len(loc):
+                    print('Выберете подразделение из списка: ')
+                    continue
+                else:
+                    sklad.moving_equ(id, division)
+                    break
+        elif mm == 0:
             break
-    except ValueError:
-        print('Введено не целое число!\n ')
+        else:
+            continue
+    except (ValueError, TypeError):
+        print('\n!!!!!!!!!Ошибка!!!!!!!!!!!!\n'
+              'Введено не верное значение!\n'
+              '!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        continue
+    except NameError:
         continue
